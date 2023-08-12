132c5be47af165e184fee48df8bfcff44198f8ed
Edu-Lefranc <105300446+EdLefranc@users.noreply.github.com>
2023-08-12 17:19:52 -0300

    Pequeña Variación.
    Se modifica el archivo ejercicioLoginFX de manera leve para comprobar su funcionamiento.

---
+++ ejercicioLoginFX/src/controlador/EjercicioLoginFX.java
@@ -0,0 +1,45 @@
+
+package controlador;
+
+import com.sun.javaws.Main;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Cristian
+ */
+public class EjercicioLoginFX extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("/vista/LoginVista.fxml"));
+            Pane ventana = (Pane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(ventana);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
